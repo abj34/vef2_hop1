de5a9093613cab1ad148bae1b2d3da296c4c22f3
abj34 <abj34@hi.is>
2023-03-13 14:54:43 +0000

    sql/types meddling

--- sql/schema.sql
+++ sql/schema.sql
@@ -19,11 +19,27 @@ CREATE TABLE public.categories (
 
 CREATE TABLE public.questions (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(64) NOT NULL,
+    title VARCHAR(64) NOT NULL UNIQUE,
     description VARCHAR(1000) DEFAULT '',
-    answer VARCHAR(1000) NOT NULL,
     category_id INTEGER NOT NULL,
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES public.categories (id) ON DELETE CASCADE
+);
+
+CREATE TABLE public.answers (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    answer VARCHAR(1000) NOT NULL,
+    fake_answer_1 VARCHAR(1000) NOT NULL,
+    fake_answer_2 VARCHAR(1000) NOT NULL,
+    fake_answer_3 VARCHAR(1000) NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT fk_questions_id FOREIGN KEY (question_id) REFERENCES public.questions (id) ON DELETE CASCADE
+);
+
+CREATE TABLE public.scores (
+    id SERIAL PRIMARY KEY,
+    -- Síðan "ALTER TABLE ADD COLUMN" þegar nýtt categories er bætt við
 );
