28a28246fb4d69b828d3652a39aa75ab49e55e14
Mikael Andri Ingason <33830730+MikaelAndriIngason@users.noreply.github.com>
2023-04-18 13:12:36 +0000

    CORS fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import { router } from './routes/api.js';
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(
+    cors({
+        origin: '*',
+    })
+)
 
 app.use(express.json());
 app.use(router);
